4c5d2213862a6e5915e8ed573a5794fc3183c256
Aleks <Aleks@Aleks-PC>
2016-11-15 23:30:58 -0700

    Creating a Collections Object

---
+++ src/CollectionObject.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+import java.util.*;
+
+public class CollectionObject implements Serializable {
+	
+	int digit1,digit2,digit3;
+	List<Integer> aList = new ArrayList<Integer>();
+	
+	public CollectionObject(){
+	}
+	
+	public CollectionObject(int digit1, int digit2, int digit3){
+		
+		this.digit1 = digit1;
+		this.digit2 = digit2;
+		this.digit3 = digit3;
+		Collections.addAll(aList, digit1, digit2, digit3);
+		Collections.sort(aList);
+	
+	}
+	
+	public List<Integer> getList(){
+		return aList;
+	}
+}
